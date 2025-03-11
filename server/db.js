77404797af7d3f2dd71ae90bf31ca488e8aa4847
Víctor Bravo <vbravo.quintana@gmail.com>
2025-03-11 13:32:38 +0000

    Cambios en la configuración de la BD en Docker

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || '127.0.0.1', 
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
